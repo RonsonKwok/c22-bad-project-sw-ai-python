5830a161d602e2173f526fe9418359c2be220ae5
Ronson Kwok <rk6866@hotmail.com>
2022-10-13 23:27:13 +0800

    updated server

--- c22-bad-project-sw-ai-project/public/ai_server.py
+++ c22-bad-project-sw-ai-project/public/ai_server.py
@@ -1,27 +1,30 @@
-from pyexpat import model
-import tensorflow as tf
-from sanic import Sanic, json
-from sanic_cors import CORS, cross_origin
-import os
-import base64
-import pathlib
-import mediapipe as mp
-import cv2
-import numpy as np
-import time
-import json as JSON
+# from pyexpat import model
+# import tensorflow as tf
+from sanic import Sanic
+from sanic.response import json
+# from sanic_cors import CORS, cross_origin
+# import os
+# import base64
+# import pathlib
+# import mediapipe as mp
+# import cv2
+# import numpy as np
+# import time
+# import json as JSON
 
-app = Sanic("flower-recognition")
-CORS(app)
-
-
-def load():
-    global model
-    model = tf.keras.models.load_model("mobile_net_v2")
+app = Sanic("eatwat7")
+# CORS(app)
 
 
-app.static("public")
+# def load():
+#     global model
+#     model = tf.keras.models.load_model("mobile_net_v2")
+
+@app.route("/")
+def test(request):
+    return json({"hello": "world"})
+# app.static("public")
 
 
 if __name__ == "__main__":
-    load()
+    app.run(host="0.0.0.0", port=5000)
