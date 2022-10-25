20f8da1ac187489445c5cd9da524caec983bfa43
Ronson Kwok <rk6866@hotmail.com>
2022-10-25 15:03:59 +0800

    update multiple server config

--- Settings.py
+++ Settings.py
@@ -4,3 +4,7 @@ IMAGE_WIDTH = 160
 MODEL_NAME="model.h5"
 UNET="unet_model"
 MOBILE_NET_V2="mobilenet_v2"
+SERVER1_LOG_FILE="./logs/ai-server1.log"
+SERVER2_LOG_FILE="./logs/ai-server2.log"
+SERVER1_PORT=8000
+SERVER2_PORT=8001
